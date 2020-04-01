ca14863701c68e70523e3ce57fde6c6089eef762
bernhard <bkellerm@student.ethz.ch>
2020-04-01 13:09:33 +0200

    deleted all AWS stacks and local amplify files setup amplify again with api, database and function

--- amplify/backend/function/resetcarinfotable/src/index.js
+++ amplify/backend/function/resetcarinfotable/src/index.js
@@ -2,9 +2,9 @@
 You can access the following resource attributes as environment variables from your Lambda function
 var environment = process.env.ENV
 var region = process.env.REGION
-var apiCarinfoCarTableName = process.env.API_CARINFO_CARTABLE_NAME
-var apiCarinfoCarTableArn = process.env.API_CARINFO_CARTABLE_ARN
-var apiCarinfoGraphQLAPIIdOutput = process.env.API_CARINFO_GRAPHQLAPIIDOUTPUT
+var apiCarinfoapiCarTableName = process.env.API_CARINFOAPI_CARTABLE_NAME
+var apiCarinfoapiCarTableArn = process.env.API_CARINFOAPI_CARTABLE_ARN
+var apiCarinfoapiGraphQLAPIIdOutput = process.env.API_CARINFOAPI_GRAPHQLAPIIDOUTPUT
 
 Amplify Params - DO NOT EDIT */
 
@@ -15,6 +15,7 @@ const fs = require('fs');
 var path = require('path');
 const region = process.env.REGION;
 const CARINFO_CARTABLE_ARN = process.env.API_CARINFO_CARTABLE_ARN;
+const ddb_table_name = process.env.API_CARINFOAPI_CARTABLE_NAME
 const docClient = new AWS.DynamoDB.DocumentClient({region});
 
 /**
@@ -22,20 +23,19 @@ const docClient = new AWS.DynamoDB.DocumentClient({region});
  */
 exports.handler = function (_, _, callback) {
     // extract table name
-    const tmp = CARINFO_CARTABLE_ARN.split('/')
-    const table_name = tmp[tmp.length - 1]
+    // const tmp = CARINFO_CARTABLE_ARN.split('/')
+    // const table_name = tmp[tmp.length - 1]
 
     // remove all records
-    removeRecords(table_name)
+    removeRecords(ddb_table_name).then(_ => {
+        const filePath = path.join(process.cwd(),'data.json')
+        const rawData = fs.readFileSync(filePath)
+        const data = JSON.parse(rawData)
 
-    // TODO: readin JSON from S3
-    const filePath = path.join(process.cwd(),'data.json')
-    const rawData = fs.readFileSync(filePath)
-    const data = JSON.parse(rawData)
-
-    writeCarInfoJsonToDB(data, table_name)
-      .then(x => {callback(null)})
-      .catch(err => {callback(null, null)})
+        writeCarInfoJsonToDB(data, ddb_table_name)
+        .then(x => {callback(null)})
+        .catch(err => {callback(null, null)})
+    })
 };
 
 
@@ -77,24 +77,30 @@ function writeCarInfoJsonToDB(json, table) {
 function removeRecords(table) {
   console.log('removeRecords')
   // read as many ids as possible (4KB limit) and then delete
-  getRecords(table).then((records) => {
-    console.log('records', records);
-    let hasError = false;
-    records.forEach(record => {
-      deleteRecord(record, table)
-        .then(() => {})
-        .catch(err=>{
-          console.log('could not delete this record', record,  err);
-          hasErrorhasError = true;
+
+  return new Promise((resolve, reject) => {
+    getRecords(table).then((records) => {
+        console.log('records', records);
+        let hasError = false;
+        records.forEach(record => {
+          deleteRecord(record, table)
+            .then(() => {})
+            .catch(err=>{
+              console.log('could not delete this record', record,  err);
+              hasErrorhasError = true;
+            });
         });
-    });
-    // repeat until no more
-    // if (!hasError) {
-    //   removeRecords(table);
-    // }
-  }).catch(err => {
-    console.log('catch after forEach', err);
+        resolve();
+        // repeat until no more
+        // if (!hasError) {
+        //   removeRecords(table);
+        // }
+      }).catch(err => {
+        console.log('remove records failed', err)
+        reject(err);
+      });
   });
+  
 }
  
 function getRecords(table) {
